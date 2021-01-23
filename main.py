de9f96d17764b9dffe0e5ba0b4ebfbed0fd89167
349044040 <s.chen.81221@gmail.com>
2021-01-23 14:28:19 +0000

    while loop

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-print ("welcome to my greeting program")
+print ("welcome to my greeting program\n")
 output = input ("whats your name! ")
 
 print("hi",output,",nice to meet you")
 input("Press enter to exit")
+nou = 1
+i = 0
+while nou<=100:
+  i = i + nou
+  nou = nou + 1
+print (i)
