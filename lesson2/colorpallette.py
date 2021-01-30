c7d02d7ab9558df1495ecb125e18947fdeb6ffba
349044040 <s.chen.81221@gmail.com>
2021-01-30 16:51:40 +0000

    do you n\know whats wrong?

--- lesson2/colorpallette.py
+++ lesson2/colorpallette.py
@@ -5,6 +5,6 @@ curses.use_default_colors
 for i in range(0, curses.colors):
   curses.init_pair(i+1, i, -1)
   stdscr.addstr(str(i+1), curses.color-pair(i+1))
-  stdscr.getch():
-curse.wrapper(window)
+  stdscr.getch()
+curses.wrapper(window)
 
