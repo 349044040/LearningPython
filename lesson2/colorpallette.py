c71c87a63c55f71439667daa2143a43cd38d9c16
349044040 <s.chen.81221@gmail.com>
2021-01-30 18:53:59 +0000

    things

--- lesson2/colorpallette.py
+++ lesson2/colorpallette.py
@@ -1,10 +1,12 @@
 import curses
+
 def window(stdscr):
-  curses.start_colors
-curses.use_default_colors
-for i in range(0, curses.colors):
-  curses.init_pair(i+1, i, -1)
-  stdscr.addstr(str(i+1), curses.color-pair(i+1))
+  curses.start_color()
+  curses.use_default_colors()
+  for i in range(0, curses.COLORS):
+    curses.init_pair(i+1, i, -1)
+    stdscr.addstr(str(i+1), curses.color_pair(i+1))
   stdscr.getch()
+
 curses.wrapper(window)
 
